c8de714b59d58482c8628e9127d5c46d5582ad4e
Rick Mendes <rmendes@intuit.com>
2012-09-28 10:19:55 -0700

    refactored to be more modular and easier to understand

--- lib/simple_deploy/stack/stack_attribute_formater.rb
+++ lib/simple_deploy/stack/stack_attribute_formater.rb
@@ -11,10 +11,10 @@ module SimpleDeploy
 
     def updated_attributes(attributes)
       updates = []
-      attributes.each do |attribute|
-        key = attribute.keys.first
+      attributes.each do |attrhash|
+        key = attrhash.keys.first
         if artifact_names.include? key
-          url_hash = cloud_formation_url attribute, attributes
+          url_hash = cloud_formation_url attrhash, attributes
           updates << url_hash
           @logger.info "Adding artifact attribute: #{url_hash}"
         end
@@ -28,24 +28,11 @@ module SimpleDeploy
       @config.artifacts
     end
 
-    def cloud_formation_url(attribute, attributes)
-      name = attribute.keys.first
-      id = attribute[name]
+    def cloud_formation_url(selected_attribute, updated_attributes)
+      name = selected_attribute.keys.first
+      id = selected_attribute[name]
 
-      bucket_match = attributes.find { |h| h.has_key? "#{name}_bucket_prefix" }
-      if bucket_match
-        bucket_prefix = bucket_match["#{name}_bucket_prefix"]
-      else
-        bucket_prefix = @main_attributes["#{name}_bucket_prefix"]
-      end
-
-      domain_match = attributes.find { |h| h.has_key? "#{name}_domain" }
-      if domain_match
-        domain = domain_match["#{name}_domain"]
-      else
-        domain = @main_attributes["#{name}_domain"]
-      end
-
+      bucket_prefix, domain = find_bucket_prefix_and_domain selected_attribute, updated_attributes
       artifact = Artifact.new :name          => name,
                               :id            => id,
                               :region        => @region,
@@ -57,5 +44,24 @@ module SimpleDeploy
       { url_parameter => artifact.endpoints['s3'] }
     end
 
+    def find_bucket_prefix_and_domain(selected_attribute, updated_attributes)
+      name = selected_attribute.keys.first
+
+      bucket_match = updated_attributes.find { |h| h.has_key? "#{name}_bucket_prefix" }
+      if bucket_match
+        bucket_prefix = bucket_match["#{name}_bucket_prefix"]
+      else
+        bucket_prefix = @main_attributes["#{name}_bucket_prefix"]
+      end
+
+      domain_match = updated_attributes.find { |h| h.has_key? "#{name}_domain" }
+      if domain_match
+        domain = domain_match["#{name}_domain"]
+      else
+        domain = @main_attributes["#{name}_domain"]
+      end
+
+      [bucket_prefix, domain]
+    end
   end
 end
