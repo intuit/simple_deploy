f5dd91c5e756121d05cc0ed489bcddb95355aaed
Brett Weaver <brett@weav.net>
2013-02-06 10:42:59 -0800

    first rev of template output mapper

---
+++ lib/simple_deploy/stack/stack_output_mapper.rb
@@ -0,0 +1,58 @@
+module SimpleDeploy
+  class StackOutputMapper
+
+    def initialize(args)
+      @config      = args[:config]
+      @environment = args[:environment]
+      @logger      = args[:logger]
+    end
+
+    def map_outputs_from_stacks(args)
+      @stacks   = args[:stacks]
+      @template = args[:template]
+      @results  = {}
+
+      merge_stacks_outputs
+
+      prune_relevant_parameters
+
+      @results
+    end
+
+    private
+
+    def merge_stacks_outputs
+      @stacks.each do |s|
+        stack = Stack.new :environment => @environment,
+                          :config      => @config,
+                          :logger      => @logger,
+                          :name        => s
+        merge_outputs stack
+      end
+    end
+
+    def merge_outputs(stack)
+      stack.outputs.each do |output|
+        key   = output['OutputKey']
+        value = output['OutputValue']
+
+        if @results.has_key? key
+          @results[key] += ",#{value}"
+        else
+          @results[key] = value
+        end
+      end
+    end
+
+    def prune_relevant_parameters
+      @results.each_key do |key|
+        @results.delete key unless template_parameters.include? key
+      end
+    end
+
+    def template_parameters
+      @parameters ||= Template.new(:file => @template).parameters
+    end
+
+  end
+end
---
+++ spec/stack/stack_output_mapper_spec.rb
@@ -0,0 +1,81 @@
+require 'spec_helper'
+
+describe SimpleDeploy::StackOutputMapper do
+  before do
+    @config_mock = mock 'config'
+    @logger_stub = stub 'logger'
+
+    stack1_outputs = [ { 'OutputKey' => 'Test1', 'OutputValue' => 'val1' },
+                       { 'OutputKey' => 'Nother', 'OutputValue' => 'another' } ]
+    stack2_outputs = [ { 'OutputKey' => 'Test2', 'OutputValue' => 'val2' },
+                       { 'OutputKey' => 'NotMe', 'OutputValue' => 'another' } ]
+    stack3_outputs = [ { 'OutputKey' => 'Test1', 'OutputValue' => 'valA' } ]
+    @stack1_stub = stub 'stack1', :outputs => stack1_outputs
+    @stack2_stub = stub 'stack2', :outputs => stack2_outputs
+    @stack3_stub = stub 'stack2', :outputs => stack3_outputs
+
+    @template_stub = stub 'template', :parameters => ["Test1", "Test2"]
+
+    @mapper = SimpleDeploy::StackOutputMapper.new :config      => @config_mock,
+                                                  :environment => 'default',
+                                                  :logger      => @logger_stub
+  end
+
+  it "should return the outputs which match parameters" do
+    SimpleDeploy::Stack.should_receive(:new).
+                        with(:environment => 'default',
+                             :config      => @config_mock,
+                             :logger      => @logger_stub,
+                             :name        => 'stack1').
+                        and_return @stack1_stub
+    SimpleDeploy::Template.should_receive(:new).
+                           with(:file => '/tmp/file.json').
+                           and_return @template_stub
+    @mapper.map_outputs_from_stacks(:stacks   => ['stack1'],
+                                    :template => '/tmp/file.json').
+            should == { 'Test1' => 'val1' }
+  end 
+
+  it "should return the outputs which match parameters from multiple stacks" do
+    SimpleDeploy::Stack.should_receive(:new).
+                        with(:environment => 'default',
+                             :config      => @config_mock,
+                             :logger      => @logger_stub,
+                             :name        => 'stack1').
+                        and_return @stack1_stub
+    SimpleDeploy::Stack.should_receive(:new).
+                        with(:environment => 'default',
+                             :config      => @config_mock,
+                             :logger      => @logger_stub,
+                             :name        => 'stack2').
+                        and_return @stack2_stub
+    SimpleDeploy::Template.should_receive(:new).
+                           with(:file => '/tmp/file.json').
+                           and_return @template_stub
+    @mapper.map_outputs_from_stacks(:stacks   => ['stack1', 'stack2'],
+                                    :template => '/tmp/file.json').
+            should == { 'Test1' => 'val1', 'Test2' => 'val2' }
+  end 
+
+  it "should concatenate multiple outputs of same name into CSV" do
+    SimpleDeploy::Stack.should_receive(:new).
+                        with(:environment => 'default',
+                             :config      => @config_mock,
+                             :logger      => @logger_stub,
+                             :name        => 'stack1').
+                        and_return @stack1_stub
+    SimpleDeploy::Stack.should_receive(:new).
+                        with(:environment => 'default',
+                             :config      => @config_mock,
+                             :logger      => @logger_stub,
+                             :name        => 'stack3').
+                        and_return @stack3_stub
+    SimpleDeploy::Template.should_receive(:new).
+                           with(:file => '/tmp/file.json').
+                           and_return @template_stub
+    @mapper.map_outputs_from_stacks(:stacks   => ['stack1', 'stack3'],
+                                    :template => '/tmp/file.json').
+            should == { 'Test1' => 'val1,valA' }
+  end 
+
+end
