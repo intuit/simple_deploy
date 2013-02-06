827924c36f19d35a2d388b5f2f21912e10c86ac8
Brett Weaver <brett@weav.net>
2013-02-06 11:54:19 -0800

    first cut at passing outputs from stacks

--- lib/simple_deploy/cli/create.rb
+++ lib/simple_deploy/cli/create.rb
@@ -20,7 +20,7 @@ EOS
           opt :attributes, "= seperated attribute and it's value", :type  => :string,
                                                                    :multi => true
           opt :stacks, "Read outputs from existing stacks", :type  => :string,
-                                                            :mulit => true
+                                                            :multi => true
           opt :environment, "Set the target environment", :type => :string
           opt :log_level, "Log level:  debug, info, warn, error", :type    => :string,
                                                                   :default => 'info'
@@ -31,26 +31,30 @@ EOS
         valid_options? :provided => @opts,
                        :required => [:environment, :name, :template]
 
-        config = Config.new.environment @opts[:environment]
-
-        attributes = parse_attributes :attributes => @opts[:attributes]
-
-        mapper = StackOutputMapper.new :environment => @opts[:environment],
-                                       :config      => config,
-                                       :logger      => logger
-
-        attributes.merge! mapper.map_outputs_from_stacks(:stacks   => stacks, 
-                                                         :template => @opts[:template])
+        @config = Config.new.environment @opts[:environment]
 
         stack = Stack.new :environment => @opts[:environment],
                           :name        => @opts[:name],
-                          :config      => config,
+                          :config      => @config,
                           :logger      => logger
 
         rescue_stackster_exceptions_and_exit do
-          stack.create :attributes => attributes,
+          stack.create :attributes => merged_outputs_and_provided_attributes,
                        :template   => @opts[:template]
         end
+      end
+
+      def merged_outputs_and_provided_attributes
+        mapped = output_mapper.map_outputs_from_stacks(:stacks   => @opts[:stacks],
+                                                       :template => @opts[:template])
+        provided = parse_attributes :attributes => @opts[:attributes] 
+        provided + mapped
+      end
+
+      def output_mapper
+        @mapper ||= StackOutputMapper.new :environment => @opts[:environment],
+                                          :config      => @config,
+                                          :logger      => logger
       end
 
       def logger
