8f7cde0ffcca34e001c329acf47888eefcf66e8a
keviny22 <kevin_young@intuit.com>
2013-02-18 16:12:48 -0800

    Please Review Moved duplicated items into globals

--- spec/stack/ssh_spec.rb
+++ spec/stack/ssh_spec.rb
@@ -13,84 +13,101 @@ describe SimpleDeploy::Stack::SSH do
                  with('test-env').
                  and_return 'test-us-west-1'
     @stack_mock.stub :attributes => { :ssh_gateway => false }
+    @options = { :config      => @config_mock,
+                 :instances   => ['1.2.3.4', '4.3.2.1'],
+                 :environment => 'test-env',
+                 :ssh_user    => 'user',
+                 :ssh_key     => 'key',
+                 :stack       => @stack_mock,
+                 :name        => 'test-stack' }
+    @task_logger_mock = mock 'task_logger'
+    @ssh_options = Hash.new
+    @task_mock.stub :logger    => @task_logger_mock,
+                    :variables => @ssh_options
   end
 
   context "when unsuccessful" do
+
     it "should return false when no running instances running" do
-      options = { :config      => @config_mock,
-                  :instances   => [],
-                  :environment => 'test-env',
-                  :ssh_user    => 'user',
-                  :ssh_key     => 'key',
-                  :stack       => @stack_mock,
-                  :name        => 'test-stack' }
-      @ssh = SimpleDeploy::Stack::SSH.new options
+      options_override = { :instances   => [] }
+      @ssh = SimpleDeploy::Stack::SSH.new @options.merge options_override
+
       @ssh.execute(:sudo    => true, :command => 'uname').should be_false
+
     end
 
     it "should return false when Capistrano command error" do
-      task_logger_mock = mock 'task_logger'
-      @ssh_options = Hash.new
-      options = { :config      => @config_mock,
-                  :instances   => ['1.2.3.4', '4.3.2.1'],
-                  :environment => 'test-env',
-                  :ssh_user    => 'user',
-                  :ssh_key     => 'key',
-                  :stack       => @stack_mock,
-                  :name        => 'test-stack' }
-      @ssh = SimpleDeploy::Stack::SSH.new options
+      @ssh = SimpleDeploy::Stack::SSH.new @options
       Capistrano::Configuration.should_receive(:new).
                                 with(:output => @logger_stub).
                                 and_return @task_mock
-      @task_mock.stub :logger    => task_logger_mock,
-                      :variables => @ssh_options
 
-      task_logger_mock.should_receive(:level=).with(3)
+      @task_logger_mock.should_receive(:level=).with(3)
       @task_mock.should_receive(:set).with :user, 'user'
       @task_mock.should_receive(:server).with('1.2.3.4', :instances)
       @task_mock.should_receive(:server).with('4.3.2.1', :instances)
-      @task_mock.should_receive(:load).with({:string=>"task :execute do\n          sudo 'a_badcommand'\n          end"})
+      @task_mock.should_receive(:load).with({:string=>"task :execute do\n          sudo 'a_bad_command'\n          end"})
       @task_mock.should_receive(:execute).and_raise Capistrano::CommandError.new 'command error'
 
-      @ssh.execute(:sudo => true, :command => 'a_badcommand').should be_false
-
+      @ssh.execute(:sudo => true, :command => 'a_bad_command').should be_false
 
     end
 
-    it "should return false when Capistrano connection error"
-    it "should return false when Capistrano generic error"
-    
+    it "should return false when Capistrano connection error" do
+      @ssh = SimpleDeploy::Stack::SSH.new @options
+      Capistrano::Configuration.should_receive(:new).
+                                with(:output => @logger_stub).
+                                and_return @task_mock
+      @task_mock.stub :logger    => @task_logger_mock,
+                      :variables => @ssh_options
 
+      @task_logger_mock.should_receive(:level=).with(3)
+      @task_mock.should_receive(:set).with :user, 'user'
+      @task_mock.should_receive(:server).with('1.2.3.4', :instances)
+      @task_mock.should_receive(:server).with('4.3.2.1', :instances)
+      @task_mock.should_receive(:load).with({:string=>"task :execute do\n          sudo 'uname'\n          end"})
+      @task_mock.should_receive(:execute).and_raise Capistrano::ConnectionError.new 'connection error'
+
+      @ssh.execute(:sudo => true, :command => 'uname').should be_false
+
+     end
+
+    it "should return false when Capistrano generic error" do
+      @ssh = SimpleDeploy::Stack::SSH.new @options
+      Capistrano::Configuration.should_receive(:new).
+                                with(:output => @logger_stub).
+                                and_return @task_mock
+
+      @task_logger_mock.should_receive(:level=).with(3)
+      @task_mock.should_receive(:set).with :user, 'user'
+      @task_mock.should_receive(:server).with('1.2.3.4', :instances)
+      @task_mock.should_receive(:server).with('4.3.2.1', :instances)
+      @task_mock.should_receive(:load).with({:string=>"task :execute do\n          sudo 'uname'\n          end"})
+      @task_mock.should_receive(:execute).and_raise Capistrano::Error.new 'generic error'
+
+      @ssh.execute(:sudo => true, :command => 'uname').should be_false
+
+    end
   end
 
   context "when successful" do
     before do
-      options = { :config      => @config_mock,
-                  :instances   => ['1.2.3.4', '4.3.2.1'],
-                  :environment => 'test-env',
-                  :ssh_user    => 'user',
-                  :ssh_key     => 'key',
-                  :stack       => @stack_mock,
-                  :name        => 'test-stack' }
-      @ssh = SimpleDeploy::Stack::SSH.new options
+      @ssh = SimpleDeploy::Stack::SSH.new @options
     end
   
     describe "when execute called" do
       before do
-        task_logger_mock = mock 'task_logger'
-        @ssh_options = Hash.new
         Capistrano::Configuration.should_receive(:new).
                                   with(:output => @logger_stub).
                                   and_return @task_mock
-        @task_mock.stub :logger    => task_logger_mock,
-                        :variables => @ssh_options
-        task_logger_mock.should_receive(:level=).with(3)
+
+        @task_logger_mock.should_receive(:level=).with(3)
         @task_mock.should_receive(:set).with :user, 'user'
         @task_mock.should_receive(:server).with('1.2.3.4', :instances)
         @task_mock.should_receive(:server).with('4.3.2.1', :instances)
       end
 
-      describe "when succesful" do
+      describe "when successful" do
         it "should execute a task with sudo" do
           @task_mock.should_receive(:load).with({:string=>"task :execute do\n          sudo 'uname'\n          end"})
           @task_mock.should_receive(:execute).and_return true
