455a4c1fc4fd7ee13df348806b2c5f68f8b1a9eb
ccloes <chad_cloes@intuit.com>
2013-02-19 09:57:10 -0800

    Updated outputs_spec.rb to have running tests.  Also updated outputs to have private block.

--- spec/cli/outputs_spec.rb
+++ spec/cli/outputs_spec.rb
@@ -3,57 +3,45 @@ require 'simple_deploy/cli'
 
 describe SimpleDeploy::CLI::Outputs do
 
-  describe 'show' do
-    before do
-      @config  = mock 'config'
-      @logger  = stub 'logger'
-      @options = { :environment => 'my_env',
-                   :log_level   => 'debug',
-                   :name        => 'my_stack' }
-      @stack   = stub :attributes => { 'foo' => 'bar', 'baz' => 'blah' }
-      SimpleDeploy::Config.stub(:new).and_return(@config)
-      @config.should_receive(:environment).with('my_env').and_return(@config)
-      SimpleDeploy::SimpleDeployLogger.should_receive(:new).
-                                       with(:log_level => 'debug').
-                                       and_return(@logger)
-      SimpleDeploy::Stack.should_receive(:new).
-                          with(:config      => @config,
-                               :environment => 'my_env',
-                               :logger      => @logger,
-                               :name        => 'my_stack').
-                          and_return(@stack)
-    end
+  before do
+    @config_object = mock 'config'
+    @config_env    = mock 'environment config'
+    @stack         = mock 'stack'
+    @logger        = stub 'logger'
+    @options       = { :environment => 'test',
+                       :log_level   => 'info',
+                       :name        => 'mytest' }
+    @data          = [{ 'OutputKey' => 'key1', 'OutputValue' => 'value1' }, 
+                      { 'OutputKey' => 'key2', 'OutputValue' => 'value2' }]
+    Trollop.stub :options => @options 
+    @config_object.stub(:environments => { 'test' => 'data' })
+    SimpleDeploy::Config.stub :new => @config_object
+    SimpleDeploy::SimpleDeployLogger.should_receive(:new).
+                                     with(:log_level => 'info').
+                                     and_return @logger
+    SimpleDeploy::Stack.should_receive(:new).
+                        with(:environment => 'test',
+                             :name        => 'mytest',
+                             :config      => @config_env,
+                             :logger      => @logger).
+                        and_return(@stack)
+    @stack.stub :outputs => @data
+    @config_object.should_receive(:environment).with('test').and_return @config_env
+    @outputs = SimpleDeploy::CLI::Outputs.new
   end
 
-  it "should successfully return the show command" do
-      subject = SimpleDeploy::CLI::Outputs.new
-      subject.should_receive(:valid_options?).
-              with(:provided => @options,
-                   :required => [:environment, :name])
-      Trollop.stub(:options).and_return(@options)
-      subject.should_receive(:puts).with('foo: bar')
-      subject.should_receive(:puts).with('baz: blah')
-      subject.show
+  it "should successfully return the show command with default values" do
+    @outputs.should_receive(:puts).with('key1: value1')
+    @outputs.should_receive(:puts).with('key2: value2')
+    @outputs.show
   end
-
-  it "should return outputs as command line arguments" do
-    outputs = SimpleDeploy::CLI::Outputs.new
-    @data = [ { 'OutputKey' => 'key1', 'OutputValue' => 'value1' }, 
-              { 'OutputKey' => 'key2', 'OutputValue' => 'value2' } ]
-
-    outputs.should_receive(:print).with('-a key1=value1 ')
-    outputs.should_receive(:print).with('-a key2=value2 ')
-    outputs.should_receive(:puts).with('')
-    outputs.command_args_output(@data)
-  end
-
-  it "should return the outputs in default format" do
-    outputs = SimpleDeploy::CLI::Outputs.new
-    @data = [ { 'OutputKey' => 'key1', 'OutputValue' => 'value1' }, 
-              { 'OutputKey' => 'key2', 'OutputValue' => 'value2' } ]
-    outputs.should_receive(:puts).with('key1: value1')
-    outputs.should_receive(:puts).with('key2: value2')
-    outputs.default_output(@data)
+ 
+  it "should successfully return the show command with as_command_args" do
+    @options[:as_command_args] = true
+    @outputs.should_receive(:print).with('-a key1=value1 ')
+    @outputs.should_receive(:print).with('-a key2=value2 ')
+    @outputs.should_receive(:puts).with('')
+    @outputs.show
   end
 
 end
