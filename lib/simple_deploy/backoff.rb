2f1e5f04e038c9c509d3d2ff14be81b83e6f719c
Shad Nygren <shad@hipster.net>
2013-03-14 21:19:59 +0000

    modify backoff.rb to slow down and avoid Rate exceeded ERROR from AWS CloudFormation

--- lib/simple_deploy/backoff.rb
+++ lib/simple_deploy/backoff.rb
@@ -2,7 +2,7 @@ module SimpleDeploy
   class Backoff
     def self.exp_periods(num_periods = 3)
       (1..num_periods).each do |n|
-        yield (1.0/2.0 * (2.0**n - 1.0)).ceil
+        yield (2.0**n).ceil
       end
     end
   end
