36a5f01c7cc8a498f7e66a5f78c22aa06f0e1e6e
ccloes <chad_cloes@intuit.com>
2013-08-15 13:24:57 -0700

    Updated to handle blank exceptions

--- lib/simple_deploy/aws/cloud_formation/error.rb
+++ lib/simple_deploy/aws/cloud_formation/error.rb
@@ -12,21 +12,26 @@ module SimpleDeploy
 
         def process
           @logger.info @exception.response.body
-          message = XmlSimple.xml_in @exception.response.body
-          message['Error'].first['Message'].each do |msg|
-            case msg
-            when 'No updates are to be performed.'
-              @logger.info msg
-            when /^Stack:(.*) does not exist$/
-              @logger.error msg
-              raise Exceptions::UnknownStack.new msg
-            else
-              @logger.error msg
-              raise Exceptions::CloudFormationError.new msg
+          if @exception.response.body != ''
+            message = XmlSimple.xml_in @exception.response.body
+            message['Error'].first['Message'].each do |msg|
+              case msg
+              when 'No updates are to be performed.'
+                @logger.info msg
+              when /^Stack:(.*) does not exist$/
+                @logger.error msg
+                raise Exceptions::UnknownStack.new msg
+              else
+                @logger.error msg
+                raise Exceptions::CloudFormationError.new msg
+              end
             end
+          else
+            @logger.error 'Blank exception'
+            @logger.error @exception.response.body
+            raise Exceptions::CloudFormationError.new @exception.response.body
           end
         end
-
       end
     end
   end
